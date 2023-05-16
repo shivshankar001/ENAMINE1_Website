6e348d6feccafabd92f306a9e77b267e54029501
ENAMINE1 <kshashwat.iit@gmail.com>
2023-05-16 17:54:37 +0530

    5th commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './Components/App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
